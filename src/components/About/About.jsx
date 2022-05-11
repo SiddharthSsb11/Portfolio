3468c9a44d8e99db6b84bfab66c9de0417a89887
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-05-11 20:02:36 +0530

    about section updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -37,14 +37,17 @@ const About = (props) => {
             </Fade>
             <Fade cascade>
               <AboutDetailWrapper>
-                <AboutDetail style={{textAlign: 'justify'}}>
+                <AboutDetail style={{textAlign: 'left'}}>
                   I'm passionately into making things and even more determined in making things better.
-                  <br /> <br />
-                  I am a developer with an array of knowledge in front end and back end software development, 
-                  languages and libraries, frameworks, databases and best code practices. 
-                  I like to merge a passion for usability and user experience with technical knowledge to create mesmerizing digital experiences....<br/>
+                  <br/>
+                  I am a full-stack developer with an array of knowledge in front end and back end software development, 
+                  languages and libraries, frameworks, databases and best code practices.<br/> 
+                  I like to merge a passion for usability and user experience with technical knowledge to create mesmerizing digital experiences.<br/>
+                  I find it rewarding to dig deep and uncover solutions to challenging problems,it's like solving a puzzle.<br/> 
+                  It's something I've always enjoyed.<br/> 
+                  When I am not coding, I identify myself as a die-hard Arsenal Footbal club fan and an ardent advocate of neo-noir genre movies and shows.<br/>
                   I love to code, play Tabla, watch & play football and I am specifically into War,
-                  Kingdoms & Empires, Civilisation...to sum up, random mysterious & historical stuffs !!
+                  Kingdoms & Empires, Civilisation,to sum up, random mysterious & historical stuffs !!
                   
                 </AboutDetail>
               </AboutDetailWrapper>
