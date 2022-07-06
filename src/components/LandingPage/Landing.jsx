b07405846977d3122d89d5b9055faf9baeee0e91
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-07-06 11:45:05 +0530

    resume updated

--- src/components/LandingPage/Landing.jsx
+++ src/components/LandingPage/Landing.jsx
@@ -52,7 +52,7 @@ const Landing = () => {
         <InfoPara>
           Focused on making creative digital experiences with slick designs. <br /> Currently converting some Words into Code.
         </InfoPara>
-        <Button value="My Resume" target="_blank" href="https://drive.google.com/file/d/1s7MQ5grm74CUPsEj81he4RMQJAAmaGo2/view?usp=sharing" size="1.3rem"></Button>
+        <Button value="My Resume" target="_blank" href="https://drive.google.com/file/d/1xoiV0fLRH8MxWDcXvdE7eyjz-y6w-ry_/view?usp=sharing" size="1.3rem"></Button>
       </HeadingWrapper>
       <LoadingWrapper href="#about" rel="noreferrer">
         <Lottie options={defaultOptions} width={50} height={50} />
